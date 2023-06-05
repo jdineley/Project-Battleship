076e2918af1673379ac447486d007da5254ce72e
James Dineley <jdineley@hotmail.com>
2023-06-05 15:59:21 +0100

    index.js neatened

--- src/index.js
+++ src/index.js
@@ -1,37 +1,6 @@
-// import GameBoard from "./modules/gameboard";
-// import GameController from "./modules/gameController";
-// import UI from "./modules/ui";
 import generateUI from "./modules/ui";
-
-// const playAgainButton = document.querySelector('.play-again button')
-// const playAgainDiv = document.querySelector('.play-again')
-
-// const game = GameBoard();
-
-// const game = GameController('James')
-
-// function startGame() {
-//     return generateUI()
-// }
-
-// const UI = startGame()
-
-// playAgainButton.addEventListener('click', (e) => {
-//     playAgainDiv.classList.add('hidden')
-//     UI = startGame()
-// })
 
 const UI = generateUI();
 
 export default UI;
 
-// game.printBoard()
-
-window.battle = {
-  game: UI.game,
-  resetBoards: UI.resetBoards,
-};
-
-// window.battle = {
-//     game
-// }
